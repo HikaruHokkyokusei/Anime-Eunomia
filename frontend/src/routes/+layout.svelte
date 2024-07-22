d807462e5a2568c2f79a809e884155d7fc1d959c
Hikaru Hokkyokusei <kutabare.eerito@gmail.com>
2024-07-21 23:41:19 -0700

    Added basic frontend code.

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import "../global.css";
+</script>
+
+<div class="CenterRowFlex MainWrapper">
+    <slot></slot>
+</div>
+
+<style>
+    .MainWrapper {
+        width: 100%;
+        height: 100%;
+    }
+</style>
